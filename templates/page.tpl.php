fe3a9332936d402f634b1648cabc755cebc03d3e
Danny Englander <highrockmedia@gmail.com>
2014-08-30 06:47:37 -0700

    fixing [#2245865]

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -221,6 +221,7 @@
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
 <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
     </section>
     <!-- /#main -->
   </div>
